2d0dbf61113e9893f51f9fd5fb2a85f2893a38e1
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-24 19:22:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts season.to_s.capitalize + ":"
   holiday.each do |holidays, supplies|
-    h = 
+    h = holidays
     end
   end
 end
