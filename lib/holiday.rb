e0725e7383e02679d8e2779a12d8a29acbc6cfa8
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-24 19:25:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   new_holiday_array=[]
-  
+  holiday
 end
 
 
