ca26f63c40c5544d89c83df014c2498e246944a2
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-24 19:21:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts season.to.
+  puts season.to_s.capitalize
   holiday.each do |holidays, supplies|
     puts "  #{holidays.capitalize}: #{supplies}"
     end
