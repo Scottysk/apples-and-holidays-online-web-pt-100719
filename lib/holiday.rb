61ff3d27c6525debbb91d3fd5dfc1d4531efc218
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-24 19:26:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       supplies.include?("BBQ")
+      new_holiday_array
 end
 
 
